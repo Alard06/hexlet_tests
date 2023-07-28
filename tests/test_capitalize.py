dbf2d9543bd098a24fae0918b82e01abb718bbd6
Alard06 <NikitaMineev2020@yandex.ru>
2023-07-28 20:57:10 +0800

    lesson 2

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,13 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция не верно работает!')
+# if capitalize('hello') != 'Hello':
+#     raise Exception('Функция не верно работает!')
 
-if capitalize('') != '':
-    raise Exception('Функция не верно работает!')
+# if capitalize('') != '':
+#     raise Exception('Функция не верно работает!')
+
+
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
 
 print('Все пройдено успешно')
